9036455e06902397b2476a5c285a08f551f4fe87
Rudinei Costa <costaaugusto177@outlook.com>
2022-07-29 03:19:28 -0300

    feat: summary e dashboard/ construido estruturas dos componentes

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,18 @@
+import { Container } from "./styles";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+
+export function Summary(){
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+    </Container>
+  );
+}
