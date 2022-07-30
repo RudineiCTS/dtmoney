bec40e944d6a56f40463e36011f17f8b0f346420
Rudinei Costa <costaaugusto177@outlook.com>
2022-07-30 13:09:11 -0300

    feat: implentação de api integrada com o front

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes(){
+    this.namespace= 'api';
+
+    this.get('/transactions', ()=> {
+      return [
+        {
+          id:1,
+          title: 'Transactions 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'food',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
